d6cb5fa0dc450edfabdbd678fe31a04980d71d43
Michael Moskowitz <mmoskowitz0402@gmail.com>
2022-11-12 11:25:55 -0600

    Added permutation functionality that iterates across string length that generates permutations for each substring of initial input string.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "permuteString.h"
 
 using namespace std;
 
 int main() {
-	string str = "ABCD";
-	int n = str.size();
-	permuteString(str,0,n-1);
+	string str, strToPermute;
+	int nPerm;
+	cout << "Please input string to evaluate anagrams that are words in the Oxford 3000: ";
+	cin >> str;
+	for (int i = 0; i <= str.length(); i++){
+		strToPermute = str.substr(0,i);
+		nPerm = strToPermute.size();
+		permuteString(strToPermute,0,nPerm-1);
+	}
 	return 0;
 }
